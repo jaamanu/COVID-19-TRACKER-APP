fb9a6f09928a80a0643108347006ec795b43e588
user <user@users-MacBook-Pro.local>
2022-12-02 04:45:22 +0200

    Test codes

--- src/test/details.test.js
+++ src/test/details.test.js
@@ -3,21 +3,20 @@ import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Details from '../components/Details';
 
-
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useLocation: () => ({
     state: {
-      country: Ghana,
-      cases: 16,
-      continent: Africa,
-      todayCases: 123,
-      todayDeath: 44,
-      recovered: 23,
-      todayRecovered: 34,
-      active: 123,
-      critical: 20,
-      population: 102,
+      country: 'Ghana',
+      cases: '16',
+      continent: 'Africa',
+      todayCases: '123',
+      todayDeaths: '44',
+      recovered: '23',
+      todayRecovered: '34',
+      active: '123',
+      critical: '20',
+      population: '102',
 
     },
   }),
